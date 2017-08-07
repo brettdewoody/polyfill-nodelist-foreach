81a8ef23e2498f46b0c8367815096156e33d15fb
Brett DeWoody <brettdewoody@gmail.com>
2017-08-07 17:45:41 +0200

    Remove author note.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * Add the forEach() method to NodeList objects
- *
- * @author Brett DeWoody <brettdewoody@gmail.com>
- */
-
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
