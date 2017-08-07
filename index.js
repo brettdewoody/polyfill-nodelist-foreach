62e2b82c7cc09cabaa31de30b8784830d0079951
Brett DeWoody <brettdewoody@gmail.com>
2017-08-07 17:43:14 +0200

    Add @author and improve check.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-if (NodeList.prototype.forEach === undefined) {
-  NodeList.prototype.forEach = Array.prototype.forEach
-}
+/**
+ * Add the forEach() method to NodeList objects
+ *
+ * @author Brett DeWoody <brettdewoody@gmail.com>
+ */
 
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
